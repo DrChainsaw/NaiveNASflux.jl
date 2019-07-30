38d1ced54dfba9e90bb804e1b7c7b1d59129187c
DrChainsaw <Christian.kyril.skarby@gmail.com>
2019-07-30 20:44:39 +0200

    Change mapchildren implementation to use new treelike_mutable macro

--- src/NaiveNASflux.jl
+++ src/NaiveNASflux.jl
@@ -7,6 +7,7 @@ import Flux.Tracker: hook
 using Statistics
 using Setfield
 using LinearAlgebra
+import InteractiveUtils: subtypes
 
 export FluxLayer, FluxParLayer, FluxDense, FluxRecurrent, FluxRnn, FluxGru, FluxLstm, FluxConvolutional, FluxConv, FluxConvTranspose, FluxDepthwiseConv, FluxTransparentLayer, FluxParInvLayer, FluxDiagonal, FluxLayerNorm, FluxParNorm, FluxBatchNorm, FluxInstanceNorm, FluxGroupNorm, FluxNoParLayer
 
@@ -27,5 +28,27 @@ include("mutable.jl")
 include("vertex.jl")
 include("pruning.jl")
 include("weightinit.jl")
+
+
+# Modified version of Flux.treelike for mutable structs which are mutable mainly because they are intended to be wrapped in MutationVertices which are not easy to create in the manner which Flux.mapchildren is designed.
+function treelike_mutable(m::Module, T, fs = fieldnames(T))
+  @eval m begin
+    Flux.children(x::$T) = ($([:(x.$f) for f in fs]...),)
+    function Flux.mapchildren(f, x::$T)
+        $([:(x.$fn = f(x.$fn)) for fn in fs]...)
+        return x
+    end
+  end
+end
+
+macro treelike_mutable(T, fs = nothing)
+  fs == nothing || isexpr(fs, :tuple) || error("@treelike_mutable T (a, b)")
+  fs = fs == nothing ? [] : [:($(map(QuoteNode, fs.args)...),)]
+  :(treelike_mutable(@__MODULE__, $(esc(T)), $(fs...)))
+end
+
+for subtype in subtypes(AbstractMutableComp)
+    @treelike_mutable subtype
+end
 
 end # module
