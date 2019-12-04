e10def17e3b2106fa8feccd3c945451dea7fedc8
DrChainsaw <Christian.kyril.skarby@gmail.com>
2019-12-05 00:05:21 +0100

    Add a few more workarounds for boilerplate stuff which Zygote should not calculate gradients for

--- src/NaiveNASflux.jl
+++ src/NaiveNASflux.jl
@@ -67,29 +67,37 @@ Flux.Zygote.@adjoint! function getfield(p::Pair, i)
     getfield(p, i), Δ -> nothing
 end
 Flux.Zygote.@nograd Dict
+Flux.Zygote.@adjoint! function convert(t::Type{T}, x::AbstractDict) where T<:AbstractDict
+    convert(t, x), Δ -> nothing
+end
+
+Flux.Zygote.@nograd mutate
+Flux.Zygote.@adjoint! function dispatch!(lm::LazyMutable, m::ResetLazyMutable, x...)
+    dispatch!(lm, m, x...), Δ -> nothing
+end
+
+function ∇getdictkey(d::AbstractDict, k, ctx, Δ)
+    grad = Flux.Zygote.grad_mut(ctx, d)
+    grad[k] = Flux.Zygote.accum(get(grad, k, nothing), Δ)
+    return (nothing, grad, nothing)
+end
 
 Flux.Zygote.@adjoint! function get!(f::Function, d::AbstractDict, k)
-    exists = true
-    fback = () -> nothing
+    # Will be replaced if ∇f is called
+    back = Δ -> ∇getdictkey(d, k, __context__, Δ)
 
     function ∇f()
-        exists = false
         res,fback = Flux.Zygote.pullback(__context__,f)
+        back = function(Δ)
+                Δd = get(Flux.Zygote.grad_mut(__context__, d), k, nothing)
+                delete!(Flux.Zygote.grad_mut(__context__, d), k)
+                fback(Δ) # Always return empty tuple due to no arg?
+                return (nothing, Δd, nothing)
+            end
         return res
     end
-
-    get!(∇f, d, k), function(Δ)
-        if exists
-            grad = Flux.Zygote.grad_mut(__context__, d)
-            grad[k] = Flux.Zygote.accum(get(grad, k, nothing), Δ)
-            return (nothing, grad, nothing)
-        else
-            Δd = get(Flux.Zygote.grad_mut(__context__, d), k, nothing)
-            delete!(Flux.Zygote.grad_mut(__context__, d), k)
-            fback(Δ) # Always return empty tuple due to no arg?
-            return (nothing, Δd, nothing)
-        end
-    end
+    return get!(∇f, d, k), back
 end
+
 
 end # module
