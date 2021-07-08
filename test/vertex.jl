09acb4a9089e1a1a3328c96aef3b4a8ef9e893a9
DrChainsaw <christian.kyril.skarby@gmail.com>
2021-07-08 22:22:38 +0200

    Add switch between complex and simple depthwiseconv strategy

--- test/vertex.jl
+++ test/vertex.jl
@@ -92,7 +92,7 @@ end
         @testset "Depthwise single layer" begin
             # just to check that I have understood the wiring of the weight
             @testset "4 inputs times 2" begin
-                inpt = inputvertex("in", 4)
+                inpt = inputvertex("in", 4, FluxConv{2}())
                 dc = mutable("dc", DepthwiseConv(reshape(Float32[1 1 1 1;2 2 2 2], 1, 1, 2, 4), Float32[0,0,0,0,1,1,1,1]), inpt)
                 @test neuron_value(dc) == [1,2,1,2,2,3,2,3]
                 @test reshape(dc(fill(10f0, (1,1,4,1))), :) == [10, 20, 10, 20, 11, 21, 11, 21]
@@ -109,7 +109,7 @@ end
             end
 
             @testset "2 inputs times 3" begin
-                inpt = inputvertex("in", 2)
+                inpt = inputvertex("in", 2, FluxConv{2}())
                 dc = mutable("dc", DepthwiseConv(reshape(Float32[1 1;2 2;3 3], 1, 1, 3, 2), Float32[0,0,1,1,2,2]), inpt)
                 @test reshape(dc(fill(10f0, (1,1,2,1))), :) == [10, 20, 31, 11, 22, 32]
                 @test Δnout!(dc => -2)
@@ -123,7 +123,7 @@ end
             end
 
             @testset "1 input times 5" begin
-                inpt = inputvertex("in", 1)
+                inpt = inputvertex("in", 1, FluxConv{2}())
                 dc = mutable("dc", DepthwiseConv(reshape(Float32.(1:5), 1, 1, 5, 1), Float32.(1:5)), inpt)
                 @test reshape(dc(fill(10f0, (1,1,1,1))), :) == [11, 22, 33, 44, 55]
                 @test Δnout!(dc=>-2)
@@ -137,7 +137,7 @@ end
             end
 
             @testset "3 inputs times 7" begin
-                inpt = inputvertex("in", 3)
+                inpt = inputvertex("in", 3, FluxConv{2}())
                 dc = mutable("dc", DepthwiseConv(reshape(repeat(Float32.(1:7), 3), 1,1,7,3), Float32.(1:21)), inpt)
                 @test reshape(dc(fill(100f0, (1,1,3,1))), :) == repeat(100:100:700, 3) .+ (1:21)
                 @test Δnout!(dc => -9) do v
@@ -158,20 +158,22 @@ end
 
         @testset "DepthwiseConv groupsize 2 into groupsize 1" begin
             
-            inpt = inputvertex("in", 4)
+            inpt = inputvertex("in", 4, FluxConv{2}())
             dc1 = mutable("dc1", DepthwiseConv((2,2), nout(inpt) => 2 * nout(inpt)), inpt)
             dc2 = mutable("dc2", DepthwiseConv((2,2), nout(dc1) => nout(dc1)), dc1)
 
             @test @test_logs (:warn, r"Could not change nout of") Δnout!(v -> 1, dc1 => 2)
-            @test [nout(dc1)] == nin(dc2) == [12]
-            @test nout(dc2) == 24 #TODO: Why so big??
+            @test [nout(dc1)] == nin(dc2) == [nout(dc2) ]== [12]
+            @test lazyins(dc1) == [1:nout(inpt)]
+            @test [lazyouts(dc1)] == lazyins(dc2) == [[1, 2, -1, 3, 4, -1, 5, 6, -1, 7, 8, -1]]
 
             # Add deterministic valuefunction which wants to do non-contiguous selection across groups
-            @test @test_logs (:warn, r"Could not change nout of") Δnout!(v -> repeat([1, 2], nout(v) ÷ 2), dc1, -2)
-            @test [nout(dc1)] == nin(dc2) == [8]
+            @test  @test_logs (:warn, r"Could not change nout of") Δnout!(v -> repeat([1, 2], nout(v) ÷ 2), dc1 => -2)
+            @test [nout(dc1)] == nin(dc2) == [nout(dc2)] == [8]
 
             @test lazyins(dc1) == [1:nout(inpt)]
-            @test [lazyouts(dc1)] == lazyins(dc2) == [[2, -1, 4, -1, 6, -1, 8, -1]]
+            # The value function above did this. Typically we would get higher weigths for non-zero parameters
+            @test [lazyouts(dc1)] == lazyins(dc2) == [[1, -1, 3, -1, 5, -1, 7, -1]]
 
             # Test that we actually succeeded in making a valid model
             y1 = dc1(ones(Float32, 3,3, nout(inpt), 2))
@@ -181,7 +183,7 @@ end
         end
 
         @testset "DepthwiseConv groupsize 3 into groupsize 5" begin
-            inpt = inputvertex("in", 4)
+            inpt = inputvertex("in", 4, FluxConv{2}())
             dc1 = mutable("dc1", DepthwiseConv((2,2), nout(inpt) => 3 * nout(inpt)), inpt)
             dc2 = mutable("dc2", DepthwiseConv((2,2), nout(dc1) => 5 * nout(dc1)), dc1)
             dc3 = mutable("dc3", DepthwiseConv((2,2), nout(dc2) => nout(dc2)), dc2)
@@ -189,7 +191,7 @@ end
             # TODO: Check compgraph output pre and post?
             # Need to insert zeros then?
 
-            @test @test_logs (:warn, r"Could not change nout of") Δnout!(v -> 1, dc1, 2)
+            @test @test_logs (:warn, r"Could not change nout of") Δnout!(v -> 1, dc1 => 2)
             @test [nout(dc1)] == nin(dc2) == [16]
             @test [nout(dc2)] == nin(dc3) == [96] # TODO: Why so big??
 
@@ -214,7 +216,7 @@ end
         end
 
         @testset "Depthwise conv change input size from Conv" begin
-            inpt = inputvertex("in", 4)
+            inpt = inputvertex("in", 4, FluxConv{2}())
             v1 = mutable("v1", Conv((1,1), nout(inpt) => 3), inpt)
             v2 = mutable("v2", DepthwiseConv((1,1), nout(v1) => 2 * nout(v1)), v1)
             v3 = mutable("v3", Conv((1,1), nout(v2) => 3 * nout(inpt)), inpt)
