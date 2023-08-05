03b1c77aa57ef40b9f7cf09140a5fc962dc69f1a
DrChainsaw <Christian.kyril.skarby@gmail.com>
2023-08-05 23:51:39 +0200

    Remove AutoOptimisers (moved to NaiveGAflux for now)

--- src/NaiveNASflux.jl
+++ src/NaiveNASflux.jl
@@ -23,8 +23,6 @@ export convinputvertex, conv1dinputvertex, conv2dinputvertex, conv3dinputvertex
 
 export ActivationContribution, LazyMutable
 
-export AutoOptimiser, optimisersetup!
-
 export layer
 
 export KernelSizeAligned
@@ -38,7 +36,6 @@ include("select.jl")
 include("mutable.jl")
 include("vertex.jl")
 include("neuronutility.jl")
-include("autooptimiser.jl")
 
 # Stuff to integrate with Zygote
 include("chainrules.jl")
@@ -50,10 +47,10 @@ using PrecompileTools
     iv1 = denseinputvertex("iv1", 1)
     v1 = fluxvertex("v1", Dense(nout(iv1) => 1), iv1)
     v2 = concat("v2", v1, v1; layerfun=ActivationContribution)
-    v3 = concat("v3", v2,v1,iv1; layerfun = AutoOptimiser)
+    v3 = concat("v3", v2,v1,iv1)
     v4 = "v4" >> v3 + v3
     v5 = "v5" >> v4 + v4 + v4
-    v6 = fluxvertex("v6", Dense(nout(v5) => 1), v5; layerfun = ActivationContribution ∘ LazyMutable ∘ AutoOptimiser)
+    v6 = fluxvertex("v6", Dense(nout(v5) => 1), v5; layerfun = ActivationContribution ∘ LazyMutable)
 
     g1 = CompGraph(iv1, v6)
     x1 = ones(Float32, 1, 1)
